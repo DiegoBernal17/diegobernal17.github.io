d8511c4539d83816928221b7d342e5e3daa4d3ce
Diego Bernal <diego.bernal17@gmail.com>
2026-08-20 14:35:41 -0600

    Add website profile to CV and include corresponding icon

--- src/utils/constants/social-icons.ts
+++ src/utils/constants/social-icons.ts
@@ -52,4 +52,20 @@ export const SOCIAL_ICONS: Record<string, string> = {
   ></path></svg
   >
 `,
+  Website: `<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="16"
+  height="16"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+  style="margin-right: 8px"
+  ><circle cx="12" cy="12" r="10"></circle><path
+    d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20"></path><path
+    d="M2 12h20"></path></svg
+>
+`,
 };
